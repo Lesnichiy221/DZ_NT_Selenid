d491eee9fcf94c613bbad2103ae1a881e3558b89
Vlad K <lesnichygpt@gmail.com>
2024-10-21 20:24:27 +0300

    update selenid version and add generateDate

--- src/test/java/ru/netology/CardTestSuccsesfuly.java
+++ src/test/java/ru/netology/CardTestSuccsesfuly.java
@@ -17,9 +17,9 @@ import static com.codeborne.selenide.Selenide.open;
 
 class CardTestSuccsesfuly {
 
-    LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String dateText = deliveryDateCard.format(formatter);
+    private String generateDate(long addDays, String pattern){
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @BeforeEach
     void setUp() {
@@ -31,14 +31,14 @@ class CardTestSuccsesfuly {
 
         $("[data-test-id=city] [placeholder='Город']").setValue("Краснодар");
         $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(generateDate(3, "dd.MM.yyyy"));
         $("[data-test-id=name] [type=text]").setValue("Ирина Пирогова");
         $("[data-test-id=phone] [type=tel]").setValue("+79883339918");
         $("[data-test-id=agreement]").click();
         $("[role=button] .button__content").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
         $("[data-test-id='notification'] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + generateDate(3, "dd.MM.yyyy")));
     }
     @Test
     void shouldReturnSuccsesfulyIfNameOrSurnameHaveDefise(){
@@ -48,14 +48,14 @@ class CardTestSuccsesfuly {
 
         $("[data-test-id=city] [placeholder='Город']").setValue("Краснодар");
         $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(generateDate(3, "dd.MM.yyyy"));
         $("[data-test-id=name] [type=text]").setValue("Ирина Пирогова-Антон");
         $("[data-test-id=phone] [type=tel]").setValue("+79883339918");
         $("[data-test-id=agreement]").click();
         $("[role=button] .button__content").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
         $("[data-test-id='notification'] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + generateDate(3, "dd.MM.yyyy")));
 
 
     }
@@ -65,14 +65,14 @@ class CardTestSuccsesfuly {
 
         $("[data-test-id=city] [placeholder='Город']").setValue("Ростов-на-Дону");
         $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(generateDate(3, "dd.MM.yyyy"));
         $("[data-test-id=name] [type=text]").setValue("Ирина Пирогова-Антон");
         $("[data-test-id=phone] [type=tel]").setValue("+79883339918");
         $("[data-test-id=agreement]").click();
         $("[role=button] .button__content").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
         $("[data-test-id='notification'] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + generateDate(3, "dd.MM.yyyy")));
 
 
     }
